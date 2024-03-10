30d320b7384096977d30d6352c342ce65284bd75
gabi-0 <gmihalcea5@gmail.com>
2024-03-10 09:35:00 +0200

    foreground service notification updated

--- app/src/main/java/com/example/basictimer/TimerService.java
+++ app/src/main/java/com/example/basictimer/TimerService.java
@@ -5,14 +5,19 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.app.StatusBarManager;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
 import android.os.Build;
 import android.os.IBinder;
+import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 
 public class TimerService extends Service {
+
+    private NotificationManager mNotification;
+
     public TimerService() {
     }
 
@@ -27,6 +32,15 @@ public class TimerService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         int k = super.onStartCommand(intent, flags, startId);
         Log.i("service", "startCmd");
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            Notification ntf = new Notification.Builder(this, NotifManager.CHANNEL_ID)
+                    .setSmallIcon(R.mipmap.web)
+                    .setContentTitle("Hello")
+                    .setContentText("22:00.0")
+                    .build();
+            mNotification.notify(NotifManager.NOTIFICATION_ID, ntf);
+        }
+        dispNotif(mNotification);
         return k;
     }
 
@@ -36,18 +50,30 @@ public class TimerService extends Service {
         try {
             NotificationChannel notificationCh = new NotificationChannel(NotifManager.CHANNEL_ID,
                     NotifManager.CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Activity.NOTIFICATION_SERVICE);
-            notificationManager.createNotificationChannel(notificationCh);
+            mNotification = (NotificationManager) getSystemService(Activity.NOTIFICATION_SERVICE);
+            mNotification.createNotificationChannel(notificationCh);
+            dispNotif(mNotification);
 
             Notification ntf = new Notification.Builder(this, NotifManager.CHANNEL_ID)
                     .setSmallIcon(R.mipmap.web)
                     .setContentTitle("Hello <3")
                     .setContentText("00:00.0")
                     .build();
+
             Log.i("app", "started");
-            startForeground(NotifManager.FOREGROUND_ID, ntf, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK);
+            startForeground(NotifManager.NOTIFICATION_ID, ntf, ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK);
         } catch(Exception e) {
             //
+        }
+    }
+
+    private void dispNotif(NotificationManager ntMngr) {
+        if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.M) return;
+
+        Log.i("notif", "---------");
+        StatusBarNotification[] notifs = ntMngr.getActiveNotifications();
+        for(StatusBarNotification n : notifs) {
+            Log.i("notif", "id"+ n.getId());
         }
     }
 
